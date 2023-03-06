748b1d70db3e2215dd8440daae4d9de04cb88fe7
adil <yasirhussain24.12@gmail.com>
2023-03-06 21:07:10 +0500

    1st commit

---
+++ calculator/app.js
@@ -0,0 +1,13 @@
+function adil(a){
+    // console.log(a)
+    var t = document.getElementById('inp')
+    t.value += a
+
+}
+function res(){
+    var a = document.getElementById('inp')
+    a.value = eval(a.value)
+}
+function clr() {
+    document.getElementById('inp').value =''
+}
